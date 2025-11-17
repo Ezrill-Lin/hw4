aa3edc2241ab75cd1f49131c0946b3ce8c077ddc
Ezrill-Lin <linxinran04@gmail.com>
2025-11-17 01:51:40 +0000

    add training start comments

--- hw4-code/part-2-code/train_t5.py
+++ hw4-code/part-2-code/train_t5.py
@@ -65,6 +65,7 @@ def train(args, model, train_loader, dev_loader, optimizer, scheduler):
     model_sql_path = os.path.join(f'results/t5_{model_type}_{experiment_name}_dev.sql')
     model_record_path = os.path.join(f'records/t5_{model_type}_{experiment_name}_dev.pkl')
     for epoch in range(args.max_n_epochs):
+        print(f"\nEpoch {epoch}: Starting training...")
         tr_loss = train_epoch(args, model, train_loader, optimizer, scheduler)
         print(f"Epoch {epoch}: Average train loss was {tr_loss}")
 
