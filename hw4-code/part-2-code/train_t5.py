5b9baa18264154506f0245a35f23055710096010
Ezrill-Lin <linxinran04@gmail.com>
2025-11-18 17:34:10 +0000

    src experiment

--- hw4-code/part-2-code/train_t5.py
+++ hw4-code/part-2-code/train_t5.py
@@ -70,7 +70,7 @@ def train(args, model, train_loader, dev_loader, optimizer, scheduler):
         print(f"Epoch {epoch}: Average train loss was {tr_loss}")
 
         print(f"Epoch {epoch}: Starting evaluation...")
-        if epoch % 4 == 0:
+        if epoch % 5 == 0:
             eval_loss, record_f1, record_em, sql_em, error_rate = eval_epoch(args, model, dev_loader,
                                                                          gt_sql_path, model_sql_path,
                                                                          gt_record_path, model_record_path)
